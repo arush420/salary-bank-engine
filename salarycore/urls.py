a4714298676090d372f74755a8e8b36c11a70865
arush420 <arushkaushik420@gmail.com>
2026-01-16 08:28:02 +0530

    landing page added

--- salarycore/urls.py
+++ salarycore/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("", include("home.urls")),  # 👈 landing page
     path('admin/', admin.site.urls),
     # path("accounts/", include("django.contrib.auth.urls")),
     path("employee/", include("employees.urls")),
